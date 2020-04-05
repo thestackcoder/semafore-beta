0602c10deedfedd90c7a0520f00f60bf1ed96e06
Fahad Ahmed <fahad@crymzee.com>
2020-04-05 17:18:14 +0500

    user model and update organization

---
+++ src/lambda/updateOrganization.js
@@ -0,0 +1,37 @@
+// organizationUpdate.js
+import mongoose from 'mongoose'
+
+// Load the server
+import db from './server'
+
+// Load the organization Model
+import Organization from './organizationModel'
+
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  
+  try {
+    // Parse the ID
+    const data = JSON.parse(JSON.parse(event.body)),
+          id = data.id,
+          organization = data.organization,
+          response = {
+            msg: "Organization successfully updated",
+            data: organization
+          }
+    
+    // Use Organization.Model and id to update 
+    await Organization.findOneAndUpdate({_id: id}, organization)
+    
+    return {
+      statusCode: 201,
+      body: JSON.stringify(response)
+    }
+  } catch(err) {
+    console.log('Organization.update', err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({msg: err.message})
+    }
+  }
+}
