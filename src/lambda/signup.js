fb10b310add03a81d56dcbc3d77572971dda1ee9
Fahad Ahmed <fahad@crymzee.com>
2020-04-06 00:33:24 +0500

    finalizing sign up

--- src/lambda/signup.js
+++ src/lambda/signup.js
@@ -2,7 +2,10 @@
 import mongoose from 'mongoose';
 
 // password encryption
-import bcrypt from 'bycriptjs';
+import bcrypt from 'bcryptjs';
+// import { createJwtCookie } from "../helpers/jwt-helper";
+import jwt from "jsonwebtoken"
+import cookie from "cookie"
 
 // Load the server
 import db from './server';
@@ -10,11 +13,33 @@ import db from './server';
 // Load the User Model
 import User from './userModel'
 
-export async function handler(event) {
+//Creating a cookie
+
+function createJwtCookie(userId, email) {
+  const secretKey =
+      "-----BEGIN RSA PRIVATE KEY-----\n" +
+      process.env.JWT_SECRET_KEY +
+      "\n-----END RSA PRIVATE KEY-----";
+
+  const token = jwt.sign({ userId, email }, secretKey, {
+      algorithm: "RS256",
+      expiresIn: "100 days",
+  });
+
+  const jwtCookie = cookie.serialize("jwt", token, {
+      secure: process.env.NETLIFY_DEV !== "true",
+      httpOnly: true,
+      path: "/",
+  });
+    
+  return jwtCookie
+}
+
+exports.handler =  async(event, context) => {
     let errorStatusCode = 500
   
     try {
-        const { email, password } = JSON.parse(event.body);
+        const { name, email, password } = JSON.parse(event.body);
 
         const existingUser = await User.findOne({ email });
         if (existingUser !== null) {
@@ -24,10 +49,13 @@ export async function handler(event) {
 
         const passwordHash = await bcrypt.hash(password, 10);
 
-        const { insertedId } = await User.insertOne({
+        const { insertedId } = await User.create({
+          name,
           email,
           password: passwordHash,
         });        
+
+        console.log(insertedId);
 
         return {
           statusCode: 200,
