558adb05c1d217b8ea261bda1daf315ef6663450
Fahad Ahmed <fahadahmed2496@gmail.com>
2020-04-18 02:28:53 +0500

    combining ui v1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import axios from 'axios';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('/.netlify/functions/readOrganization')
+      .then((data) => {
+        // setUserSession(response.data.token, response.data.user);
+        var res = data.data.data;
+        // let arr = res.map(obj => Object.values(obj));
+        this.setState({ loading: false, msg: data.data.msg, orgs: res });
+        console.log(this.state.orgs);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  deleteRow = (id) => {
+    console.log(id);
+    axios.delete('/.netlify/functions/deleteOrganization', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        "_id": id
+      }
+    })
+      .then((data) => {
+        // setUserSession(response.data.token, response.data.user);
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -40,7 +47,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <LambdaDemo />
+          <button onClick={() => this.deleteRow("5e85c32603da8000073351ba")} className="btn btn-danger btn-sm">Delete</button>
         </header>
       </div>
     )
